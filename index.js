cb7069665d42f14ea20bc42ecbcdbddb60dba226
Art4 <art4@wlabs.de>
2017-09-06 16:59:29 +0200

    implement valuestringparser

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.valuestringparse = function(string) {
-    return string+' works!';
+exports.valuestringparse = function(input) {
+    var input = input.replace(/^\//, '').replace(/\/$/, '');
+    var parts = input.split('/');
+    if (parts.length%2 !== 0) {
+        parts.pop();
+    }
+    var data = {};
+    for (var i = 0; i < parts.length; i = i+2) {
+        var key = parts[i];
+        var value = parts[i+1];
+        data[key] = value;
+    }
+    return data;
 }
--- test/parse.js
+++ test/parse.js
@@ -2,7 +2,28 @@ var assert = require('assert');
 var parser = require('../index.js');
 
 describe('valuestringparse', function() {
-    it('should return string with somthing appended', function() {
-        assert.equal('it works!', parser.valuestringparse('it'));
+    it('should return object parsed from a string', function() {
+        assert.deepEqual(
+            {key1:'value1'},
+            parser.valuestringparse('key1/value1')
+        );
+    });
+    it('should return object parsed from a string', function() {
+        assert.deepEqual(
+            {key1:'value1',key2:'value2'},
+            parser.valuestringparse('key1/value1/key2/value2')
+        );
+    });
+    it('should return object parsed from a string', function() {
+        assert.deepEqual(
+            {key1:'value1',key2:'value2'},
+            parser.valuestringparse('/key1/value1/key2/value2/')
+        );
+    });
+    it('should return object parsed from a string', function() {
+        assert.deepEqual(
+            {key1:'value1',key2:'value2'},
+            parser.valuestringparse('/key1/value1/key2/value2/ignorethiskey/')
+        );
     });
 });
