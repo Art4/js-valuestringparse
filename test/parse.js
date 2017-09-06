d609239430a702a9309fa5f9b3fc78ee4e971d67
Art4 <art4@wlabs.de>
2017-09-06 17:04:30 +0200

    Add test case

--- test/parse.js
+++ test/parse.js
@@ -26,4 +26,10 @@ describe('valuestringparse', function() {
             parser.valuestringparse('/key1/value1/key2/value2/ignorethiskey/')
         );
     });
+    it('should return object parsed from a string', function() {
+        assert.deepEqual(
+            {make:'1',model:'3',fuel:'3',gearbox:'3',firstRegistration:'2015-01',priceRange:'9996,79000',powerRange:'71,350'},
+            parser.valuestringparse('/make/1/model/3/fuel/3/gearbox/3/firstRegistration/2015-01/priceRange/9996,79000/powerRange/71,350')
+        );
+    });
 });
